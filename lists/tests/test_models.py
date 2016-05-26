81c0838405f131a747013ecb60a8fe28c0fdfa49
will <will.flwrs@gmail.com>
2016-05-26 18:07:42 -0400

    Rename and position unit tests

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,23 +1,6 @@
-from django.core.urlresolvers import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-from django.http import HttpResponse
-from django.template.loader import render_to_string
-
-from lists.views import home_page
 from lists.models import Item, List
 
-
-class HomePageTest(TestCase):
-
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
-
-	def test_home_page_returns_correct_html(self):
-		request = HttpRequest() #
-		response = home_page(request) #
-		expected_html = render_to_string('home.html', request=request)
 
 
 class ListAndItemModelsTest(TestCase):
